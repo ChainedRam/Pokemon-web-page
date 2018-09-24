8b2f5d34908606d802cf7b303776f6e6b01eb30b
Monstring <monstring1@hotmail.com>
2018-09-24 12:57:22 +0300

    buttons can be checked and unchecked

--- src/Components/FilterMode.tsx
+++ src/Components/FilterMode.tsx
@@ -4,15 +4,52 @@ import { Button, ButtonGroup } from "reactstrap";
 export interface IFilterProps {
   filteredSelection: string[];
 }
+export interface IFilterModeState {
+  checkBox: ICheckBoxTypesState[];
+}
+export interface ICheckBoxTypesState {
+  type: string;
+  isChecked: boolean;
+}
 
-export default class FilterMode extends React.PureComponent<IFilterProps, {}> {
+export default class FilterMode extends React.PureComponent<
+  IFilterProps,
+  IFilterModeState
+> {
+  //   type = ["fire", "water", "grass"];
+  //   dictElement = {};
+  //   typeDict = this.type.forEach(e => (this.dictElement[e] = false));
+  //   componentDidMount = () => {
+  //     this.setState({ checkBox: this.typeDict });
+  //   };
+
+  componentDidMount() {
+    this.setState({ checkBox: this.checkBoxTypes });
+  }
+  checkBoxTypes = [
+    { type: "fire", isChecked: false },
+    { type: "water", isChecked: false },
+    { type: "grass", isChecked: false }
+  ];
+
+  onClicked(boxNumber: number) {
+    let checkBoxTypesCopy = [...this.checkBoxTypes];
+    checkBoxTypesCopy[boxNumber].isChecked = !checkBoxTypesCopy[boxNumber]
+      .isChecked;
+    this.setState({ checkBox: checkBoxTypesCopy });
+  }
   public render() {
-    let type = ["fire", "water", "grass"];
     return (
       <div>
         <ButtonGroup>
-          {type.map((t, i) => (
-            <Button key={i}>{t}</Button>
+          {this.checkBoxTypes.map((t, i) => (
+            <Button
+              color={t.isChecked ? "primary" : "secondary"}
+              key={i}
+              onClick={() => this.onClicked(i)}
+            >
+              {t.type}
+            </Button>
           ))}
         </ButtonGroup>
       </div>
