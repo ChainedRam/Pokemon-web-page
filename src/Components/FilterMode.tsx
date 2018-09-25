3d3b0355d5b9f227a85d69d68c8d38b5c988b3ee
Monstring <monstring1@hotmail.com>
2018-09-25 09:39:13 +0300

    Checkboxes are limited to 2 at a time

--- src/Components/FilterMode.tsx
+++ src/Components/FilterMode.tsx
@@ -16,26 +16,30 @@ export default class FilterMode extends React.PureComponent<
   IFilterProps,
   IFilterModeState
 > {
-  //   type = ["fire", "water", "grass"];
-  //   dictElement = {};
-  //   typeDict = this.type.forEach(e => (this.dictElement[e] = false));
-  //   componentDidMount = () => {
-  //     this.setState({ checkBox: this.typeDict });
-  //   };
-
   componentDidMount() {
     this.setState({ checkBox: this.checkBoxTypes });
   }
   checkBoxTypes = [
     { type: "fire", isChecked: false },
     { type: "water", isChecked: false },
-    { type: "grass", isChecked: false }
+    { type: "grass", isChecked: false },
+    { type: "dark", isChecked: false },
+    { type: "normal", isChecked: false }
   ];
 
   onClicked(boxNumber: number) {
     let checkBoxTypesCopy = [...this.checkBoxTypes];
     checkBoxTypesCopy[boxNumber].isChecked = !checkBoxTypesCopy[boxNumber]
       .isChecked;
+    let flagChecks = checkBoxTypesCopy.filter(c => c.isChecked);
+    let filterCount = flagChecks.length;
+    console.log(filterCount);
+    if (filterCount > 2) {
+      checkBoxTypesCopy[boxNumber].isChecked = !checkBoxTypesCopy[boxNumber]
+        .isChecked;
+      return;
+    }
+
     this.setState({ checkBox: checkBoxTypesCopy });
   }
   public render() {
