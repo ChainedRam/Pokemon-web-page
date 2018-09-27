d6f3ce5426895c868bd0b979a600d3fc75503420
Monstring <monstring1@hotmail.com>
2018-09-27 12:34:26 +0300

    pokemon objects each now have a list of learnable moves and abilites

--- mining/gatherPokemons.ts
+++ mining/gatherPokemons.ts
@@ -22,21 +22,32 @@ let pokeList: Pokemon[] = [];
       name: results[i].name,
       types: [],
       moves: [],
-      abilities: null
+      abilities: []
     };
     pokeList.push(pokemon);
     let url = results[i].url;
     let response = await fetch(url);
     let json = await response.json();
     let types = json.types as any[];
+    let abilities = json.abilities as any[];
+    let moves = json.moves as any[];
 
-    for (let j = 0; j < types.length; j++) {
-      let type = json.types[j].type.name;
+    for (let movesLoop = 0; movesLoop < moves.length; movesLoop++) {
+      let move = json.moves[movesLoop].move.name;
+      pokemon.moves.push(move);
+    }
+
+    for (let abilityLoop = 0; abilityLoop < abilities.length; abilityLoop++) {
+      let ability = json.abilities[abilityLoop].ability.name;
+      pokemon.abilities.push(ability);
+    }
+    for (let typeLoop = 0; typeLoop < types.length; typeLoop++) {
+      let type = json.types[typeLoop].type.name;
       pokemon.types.push(type);
     }
     let outDir = "./src/data";
 
-    exportJson(join(outDir, "pokeDict.json"), pokeList);
+    exportJson(join(outDir, "pokemons.json"), pokeList);
   }
 })().catch(e => {
   console.log(e);
